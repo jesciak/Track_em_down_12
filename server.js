47049eddfdd3bf9a7e8290b3c8370420f5fbc773
jesciak <jesciak@yahoo.com>
2022-11-12 16:20:23 -0500

    node app functioning on all prompts

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-// const mysql = require('mysql2');
-
 const inquirer = require('inquirer');
-const db= require('./connection');
+const db = require('./connection');
 const consoleTable = require('console.table');
 
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     port: '3306',
-//     user: 'root',
-//     password: 'trojans',
-//     database: 'trackem_db'
 
-// });
 db.connect(function (err) {
     if (err) throw err;
     start();
@@ -36,58 +27,61 @@ function start() {
         .then(function (answer) {
             switch (answer.option) {
                 case 'View All Employees':
-                    viewEmployees();
-                    break;
+                    return viewEmployees();
                 case 'Add Employee':
-                    addEmployee();
-                    break;
+                    return addEmployee();
                 case 'Update Employee Role':
-                    updateRole();
+                    return updateRole();
                 case 'View All Roles':
-                    viewRoles();
+                    return viewRoles();
                 case 'Add Role':
-                    addRole();
+                    return addRole();
                 case 'View All Departments':
-                    viewDepartments();
+                    return viewDepartments();
                 case 'Add Department':
-                    addDepartment();
+                    return addDepartment();
                 case 'Quit':
-                    quit();
-                    break;
+                default:
+                    return quit();
+
             }
         })
 }
 function viewEmployees() {
     db.query('SELECT * FROM employee', function (err, res) {
         if (err) throw err;
+        console.log('***********************************************************');
+        console.log(`Viewing ${res.length} employees`);
+        console.log('***********************************************************');
         console.table(res);
+        console.log('***********************************************************');
+
         start();
     })
 }
 function viewDepartments() {
     db.query('SELECT * FROM department', (err, res) => {
         if (err) throw err;
+        console.log('***********************************************************');
+        console.log(`Viewing ${res.length} departments`);
+        console.log('***********************************************************');
         console.table(res);
+        console.log('***********************************************************');
         start();
     })
 }
-function addDepartment() {
-    inquirer.prompt([
-        {
-            name: 'deptAdd',
-            type: 'input',
-            message: 'What is the name of the department?'
-        }
-    ])
-        .then(function (answer) {
-            db.query('INSERT INTO department (name) VALUES (?)', answer.deptAdd, function (err, res) {
-                if (err) throw err;
-                console.log(`Added ${answer.deptAdd} to the database`);
-                console.table(res);
-                start();
-            })
-        })
-    }
+function viewRoles() {
+    db.query('SELECT * FROM role', function (err, res) {
+        if (err) throw err;
+        console.log('***********************************************************');
+        console.log(`Viewing ${res.length} roles`);
+        console.log('***********************************************************');
+        console.table(res);
+        console.log('***********************************************************');
+
+        start();
+    });
+};
 function addEmployee() {
     db.query('SELECT * FROM role', function (err, res) {
         if (err) throw err;
@@ -130,24 +124,88 @@ function addEmployee() {
                     },
                     function (err) {
                         if (err) throw err;
+                        console.table(res);
+                        console.log('***********************************************************');
                         console.log(`Added ${answer.first_name} ${answer.last_name} to the database as a ${answer.role}`);
+                        console.log('***********************************************************');
+
                         start();
                     })
-                })
+            })
+    })
+}
+function addDepartment() {
+    inquirer.prompt([
+        {
+            name: 'deptAdd',
+            type: 'input',
+            message: 'What is the name of the department?'
+        }
+    ])
+        .then(function (answer) {
+            db.query('INSERT INTO department (name) VALUES (?)', answer.deptAdd, function (err, res) {
+                if (err) throw err;
+                console.log('***********************************************************');
+                console.log(`Added ${answer.deptAdd} to the database`);
+                console.log('***********************************************************');
+                start();
+            })
         })
-    }
-    function viewRoles() {
-        db.query('SELECT * FROM role', function (err, res) {
-            if (err) throw err;
-            console.table(res);
-            start();
-        });
-    };
-function updateRole() {
-    // var roleSelection = 'SELECT * FROM role;';
-    // var deptSelection = 'SELECT * FROM department;';
-    let newRole = {};
+}
+function addRole() {
+    let addedRole = {};
+    db.query('SELECT * FROM department', function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: 'roleAdd',
+                type: 'input',
+                message: 'What is the name of the role?'
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'What is the salary of the role?'
+            },
+            {
+                name: 'dept',
+                type: 'list',
+                message: "Which department does the role belong to?",
+                choices: function () {
+                    let deptArray = [];
+                    for (let i = 0; i < res.length; i++) {
+                        deptArray.push(res[i].name);
+                    }
+                    return deptArray;
+                },
+            }
+        ])
+            .then(function (answer) {
+                let deptId;
+                for (let i = 0; i < res.length; i++) {
+                    if (res[i].name == answer.dept) {
+                        deptId = res[i].id;
+                    }
+                }
+                db.query('INSERT INTO role SET ?',
+                    {
+                        title: answer.roleAdd,
+                        salary: answer.salary,
+                        department_id: deptId
 
+                    },
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log('***********************************************************');
+                        console.log(`Added ${answer.roleAdd} to the database`);
+                        console.log('***********************************************************');
+                        start();
+                    })
+            })
+    })
+}
+function updateRole() {
+    let newRole = {};
     db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, e2.first_name AS manager FROM employee LEFT JOIN employee AS e2 ON e2.id= employee.manager_id JOIN role ON employee.role_id = role.id JOIN department ON role.department_id= department.id ORDER BY employee.id', function (err, res) {
         if (err) throw err;
         inquirer.prompt([
@@ -186,9 +244,11 @@ function updateRole() {
                             db.query('SELECT * FROM role WHERE title = ?', answer.updateRole, function (err, res) {
                                 if (err) throw err;
                                 newRole.role_id = res[0].id;
-                                db.query('UPDATE employee SET role_id = ? WHERE first_name = ?', [newRole.role_id, newRole.first_name], function (err, res) {
+                                db.query('UPDATE employee SET role_id = ? WHERE first_name = ?', [newRole.role_id, newRole.employee_id], function (err, res) {
                                     if (err) throw err;
-                                    console.log("Updated employee's role successfully")
+                                    console.log('***********************************************************');
+                                    console.log(`Updated ${newRole.first_name}'s role to ${answer.updateRole} successfully`);
+                                    console.log('***********************************************************');
                                     start();
                                 })
                             })
@@ -198,51 +258,6 @@ function updateRole() {
             })
     })
 }
-
-function addRole() {
-    let addedRole = {};
-    db.query('SELECT * FROM department', function (err, res) {
-        if (err) throw err;
-        inquirer.prompt([
-            {
-                name: 'roleAdd',
-                type: 'input',
-                message: 'What is the name of the role?'
-            },
-            {
-                name: 'salary',
-                type: 'input',
-                message: 'What is the salary of the role?'
-            },
-            {
-                name: 'dept',
-                type: 'list',
-                choices: function () {
-                    let deptArray = [];
-                    for (let i = 0; i < res.length; i++) {
-                        deptArray.push(res[i].name);
-                    }
-                    return deptArray;
-                },
-                message: "Which department does the role belong to?"
-            }
-        ])
-            .then(function (answer) {
-                addedRole.title = answer.roleAdd;
-                addedRole.salary = answer.salary;
-
-                db.query('SELECT id FROM department WHERE name = ?', answer.dept, function (err, res) {
-                    if (err) throw err;
-                    addedRole.department_id = res[0].id;
-                    console.log(`Added ${answer.roleAdd} to the database`);
-                    start();
-                })
-
-            })
-
-    })
-}
-
 function quit() {
     db.end();
 }
